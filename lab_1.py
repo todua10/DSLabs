2c7fed26abb82ff81b110d598c1132e6b9a8c0c4
todua10 <todua10@inbox.ru>
2021-03-02 10:57:41 +0500

    Creating a solution to task 2 and a simple task selection menu

--- lab_1.py
+++ lab_1.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def Array_abs_neg():
+def Index_abs_neg():
     n = int(input("Введите размер массива: "))
     array = []
     print("Введите числа:")
@@ -14,3 +14,21 @@ def Array_abs_neg():
             index_min_abs_neg = i
             min_abs_neg = abs(array[i])
     print(index_min_abs_neg)
+
+
+def Overlapping_strings():
+    origin_str = input("Введите оригинальную строку: ")
+    overlapping_str = input("Введите строку для перекрытия: ")
+    num_of_char = int(input("Введите кол-во символов для перекрытия строки: "))
+    position = int(input("Введите позицию в оригинальной строке: "))
+    result_str = origin_str[:position - 1] + overlapping_str[:num_of_char] + origin_str[position + num_of_char - 1:]
+    print(result_str)
+
+
+task = int(input("Введите задание, которое следует выполнить: 1 или 2? \n"))
+if task == 1:
+    Index_abs_neg()
+elif task == 2:
+    Overlapping_strings()
+else:
+    print("Введено неверное значение!")
